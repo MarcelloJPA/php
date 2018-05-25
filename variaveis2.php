7748758a28c5f903aaf613b2583622e2c2a2d824
MarcelloJPA <cellolim4jpa@gmail.com>
2018-05-24 22:32:52 -0300

    go

---
+++ variaveis2.php
@@ -0,0 +1,20 @@
+<?php
+		$nome = "Marcello";           //STRING
+		$site = "www.google.com.br";	//STRING
+		$ano = 1993;					//INT
+		$salario = 5800.00;				//REAL
+		$bloqueado = false;				//BOOLEANO
+
+		$frutas = array("maça", "banana", "maracujá", "melão","laranja");
+
+		echo $frutas[4];
+
+
+		$nascimento = new Datetime();
+
+		var_dump($nascimento);
+
+
+
+
+?>
