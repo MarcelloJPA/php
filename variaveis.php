9d5ee71cc4287a9d603b4906fa4b0217b144ef5a
MarcelloJPA <cellolim4jpa@gmail.com>
2018-05-24 22:12:57 -0300

    2

--- variaveis.php
+++ variaveis.php
@@ -1,7 +1,28 @@
 <?php
 
 $nome = "Marcello lima";
+
 $idade = 25;
+
+$sobrenome = "da Costa";
+
+$nomeCompleto = $nome . " " . $sobrenome;  //CONCATENAÇÃO
+echo $nomeCompleto;
+
+l
+unset($nome);  // APAGA CONTEUDO DA VARIÁVEL 
+
+echo "<br>";
+
+  
+
+if(isset($nome)){     //"ISSET"-> se existe conteudo na variavel $nome imprima senão informe q esta vazia 
+	echo $nome;
+}
+else {
+	echo"variavel está vazia";
+}
+
 
 
 ?>
